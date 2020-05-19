c9e06adb0c808738201f6ec0374bf7a808fbdee8
surmistry <mistrs8@gmail.com>
2020-05-19 18:15:37 -0400

    Implement weather api call

--- weather.js
+++ weather.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
+const weather = require('weather-js');
+
+// Execute the following line to create a MacOS executable file from ./weather.js
+
+// $ chmod +x ./weather.js
 
 // Convert weather script into IIFE to retain propper context in the terminal shell
 (async () => {
+
 
   const checkForPostCode = (check) => {
     const postExp = /^.*[0-9]/;
@@ -24,5 +30,18 @@
     const args = process.argv.slice(2);
     return args.length > 0 ? (sanitizeInputs(args)) : false;
   }
+
+
+  const findWeather = (city, units = "C") => new Promise((resolve, reject) => {
+    weather.find(
+      { search: city, degreeType: units },
+      (err, result) => {
+        if (err) resolve({ err: true, message: 'Could not find the city: ' });
+        (result && result[0]) ? resolve(result[0].current) : resolve(`City ${city} couldn't be found`);
+      })
+  });
+
+  [inputCity] = handleArguments(process);
+  const answers = await findWeather(inputCity);
 
 })()
