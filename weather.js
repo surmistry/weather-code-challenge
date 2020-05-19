22ef61c8be27dccdc2fa276b897d46faa937e90b
surmistry <mistrs8@gmail.com>
2020-05-19 18:09:37 -0400

    Sanitize inputs from cli

--- weather.js
+++ weather.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
 
+// Convert weather script into IIFE to retain propper context in the terminal shell
+(async () => {
+
+  const sanitizeInputs = (cities, separator = ',') => {
+    const allCities = cities.join(' ');
+    const separatedCities = allCities.split(separator);
+    const specifiedCities = separatedCities.reduce((aggregateCities, currentCity, index) => {
+      let newCities = aggregateCities;
+      if (checkForPostCode(currentCity)) newCities[newCities.length - 1] = `${newCities[newCities.length - 1]} ${currentCity}`
+      else newCities.push(currentCity)
+      return newCities
+    }, [])
+    return specifiedCities;
+  }
+
+  const handleArguments = (process) => {
+    const args = process.argv.slice(2);
+    return args.length > 0 ? (sanitizeInputs(args)) : false;
+  }
+
+})()
